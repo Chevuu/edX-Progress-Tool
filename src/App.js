d94f0152d74dca0f949ddd90d07c07a88bd6dfb8
Vuk <vukjurisic@gmail.com>
2024-09-25 12:36:13 +0200

    dynamic checklist

--- src/App.js
+++ src/App.js
@@ -1,63 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 import Checklist from './components/Checklist';
 import './App.css';
 import './styles/admin.css';
 import './styles/modal.css';
 
-// function ChecklistPage() {
-//   const { courseCode, courseRun, checklistID } = useParams();
-//   const [checklist, setChecklist] = useState(null);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
+function ChecklistPage() {
+  const { courseCode, courseRun, checklistID } = useParams();
 
-//   useEffect(() => {
-//     fetch(`/progress-tool/edX-Progress-Tool/server/file_name_here_php`)
-//       .then(response => {
-//         if (!response.ok) {
-//           throw new Error('Error fetching checklist');
-//         }
-//         return response.json();
-//       })
-//       .then(data => {
-//         const parsedQuestions = JSON.parse(data.Questions).map((question, index) => ({
-//           id: index,
-//           task: question,
-//           isChecked: JSON.parse(data.Checks)[index],
-//         }));
-//         setChecklist(parsedQuestions);
-//         setLoading(false);
-//       })
-//       .catch(error => {
-//         setError(error.message);
-//         setLoading(false);
-//       });
-//   }, [courseCode, courseRun, checklistID]);
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>Error: {error}</div>;
-//   }
-
-//   return (
-//     <div className="App">
-//       {checklist ? (
-//         <Checklist items={checklist} courseCode={courseCode} courseRun={courseRun} checklistID={checklistID} />
-//       ) : (
-//         <p>No checklist available</p>
-//       )}
-//     </div>
-//   );
-// }
+  return (
+    <Checklist courseCode={courseCode} courseRun={courseRun} checklistID={checklistID} />
+  );
+}
 
 function App() {
   return (
-    <div>
-      <Checklist courseCode="1" courseRun="2" checklistID="3" />
-    </div>
+    <Router>
+      <Routes>
+        {/* Define the route with parameters */}
+        <Route path="/checklist/:courseCode/:courseRun/:checklistID" element={<ChecklistPage />} />
+        {/* Other routes can go here */}
+      </Routes>
+    </Router>
   );
 }
 
